36f52f249183cbdb8cb3df0d586fef681cf164b9
Aman <amanchdurga086@gmail.com>
2026-07-20 20:05:19 +0530

    Create User model with password hashing middleware

---
+++ models/User.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+      select: false,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// pre middleware (or a pre hook)
+userSchema.pre("save", async function (next) {
+    // Arrow functions don't have their own this
+  if (!this.isModified("password")) {
+    // Checks whether the password field has changed.
+    return next();
+    // Skips the hashing logic and continues saving the document.
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+//   Passes control to the next middleware (or the actual save operation).
+});
+
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
