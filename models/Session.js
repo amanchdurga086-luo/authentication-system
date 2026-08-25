7c6a10f6a0579c2f3c65ee8479823a7f8889d5b3
Aman <amanchdurga086@gmail.com>
2026-08-25 23:33:50 +0530

    Add session model for refresh token authentication

---
+++ models/Session.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const sessionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+
+    refreshTokenHash: {
+      type: String,
+      required: true,
+    },
+
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+
+    revokedAt: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Session = mongoose.model("Session", sessionSchema);
+
+module.exports = Session;
